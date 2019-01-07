db938c8fe5d176030eb21d72fe91f2e847da2743
Joshua Lavarine <joshua@lavarine.com>
2019-01-07 14:00:49 -0700

    Create delete card functionality

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // --------------------GLOBAL VARIABLES--------------------
 var addToAlbum = document.querySelector(".add-img-btn");
-var photoInput = document.querySelector(".choose-file-btn")
+var photoInput = document.querySelector(".choose-file-btn");
 var titleInput = document.querySelector(".title-input");
 var captionInput = document.querySelector(".caption-input");
 var photoGallery = document.querySelector(".album-field");
-var deleteButton = document.querySelector("#delete-btn");;
+var deleteButton = document.querySelector("#delete-btn");
 var cardSection = document.querySelector(".album-field");
 var viewFavorites = document.querySelector(".view-fav-btn");
 var imagesArr = JSON.parse(localStorage.getItem("imagesArr")) || [];
@@ -12,25 +12,25 @@ var reader = new FileReader();
 
 
 // --------------------EVENT LISTENERS---------------------
-
 window.addEventListener('load', appendPhotos(imagesArr));
-addToAlbum.addEventListener("click", createElement);
+addToAlbum.addEventListener("click", stringPhotos);
 cardSection.addEventListener("click", manipulateCard);
 
 // --------------------FUNCTIONS---------------------------
 //Persist
 function appendPhotos(array) {
+  //if this isn't here, I get extra imagesArr array items 
   imagesArr = [];
   array.forEach(function (card) {
     displayPhotoCard(card);
-    const newPhoto = new Photo(card.id, card.title, card.caption, card.file, card.favorite);
+    var newPhoto = new Photo(card.id, card.title, card.caption, card.file, card.favorite);
+    // console.log(newPhoto);
     imagesArr.push(newPhoto);
   })
 }
 
 //Don't mess with this one (photo magic happening)
-function createElement(e) {
-  // console.log(input.files[0])
+function stringPhotos(e) {
   e.preventDefault();
   if (photoInput.files[0]) {
     reader.readAsDataURL(photoInput.files[0]); 
@@ -40,7 +40,7 @@ function createElement(e) {
 
 function addPhoto(e) {
   // console.log(e.target.result);
-  var newPhoto = new Photo(Date.now(), titleInput.value, captionInput.value, e.target.result);
+  var newPhoto = new Photo(Date.now(), e.value, e.value, e.target.resul);
   displayPhotoCard(newPhoto);
   imagesArr.push(newPhoto);
   newPhoto.saveToStorage();
@@ -72,45 +72,86 @@ function displayPhotoCard(object) {
       </article>
     `;
 }
-
+//keep functions under 10 lines
 
 function manipulateCard(e) {
   e.preventDefault();
-  var uniqueID = event.target.parentElement.parentElement.parentElement.dataset.id;
-  console.log(uniqueID);
+  var uniqueId = event.target.parentElement.parentElement.parentElement.dataset.id;
+  var parsedId = parseInt(uniqueId);
+  // console.log(parsedId);
   var index = imagesArr.findIndex(function(photo) {
-    return photo.id === uniqueID;
+    return photo.id === parsedId;
   })
   if (event.target.classList.contains("delete")) {
+    console.log(event.target);
     deletePhotoCard();
 }}
 
 
-function deletePhotoCard() {
-  var uniqueID = event.target.parentElement.parentElement.parentElement.dataset.id;
-  console.log(uniqueID);
-  var index = imagesArr.findIndex(function(photo) {
-    return photo.id === parseInt(uniqueID);
-  });
-  imagesArr[index].deleteFromStorage();
-  event.target.closest(dataset.id).remove();
-}
+// function deletePhotoCard(id) {
+//   var uniqueId = parseInt(event.target.parentElement.parentElement.parentElement.dataset.id);
+//   console.log(uniqueId)
+//   console.log(id);
+//   var newPhoto = new Photo(uniqueId.id, uniqueId.title, uniqueId.caption, uniqueId.file, uniqueId.favorite);
+//   // var parsedId = JSON.parse(localStorage.getItem(uniqueId));
+//   // console.log(parsedId, 'should be here');
+//   // var selectedId = document.querySelector("")
+//   newPhoto.remove();
+//   var index = imagesArr.findIndex(function(image) {
+//     return image === id.id;
+//   });
+//   index.deleteFromStorage(imagesArr, index.id);
+// }
 
   // if (event.target.classList.contains("favorite")) {
   //   editCard();
 //   })
 // }
 
-
 // function deletePhotoCard(e) {
 //   e.preventDefault();
 //   var deleteButton = document.querySelector("#delete-btn");
 //   deleteButton.addEventListener("click", deletePhotoCard);
-//   var uniqueID = event.target.closest.dataset.id;
+//   var uniqueId = event.target.closest.dataset.id;
 //   var index = imagesArr.findIndex(function(photo) {
-//     return photo.id === parseInt(uniqueID);
+//     return photo.id === parseInt(uniqueId);
 //   });
 //   imagesArr[index].deleteFromStorage();
 //   imagesArr.splice(index, 1);
 //   event.target.closest(dataset.id).remove();
 // }
+
+
+// //Duy help
+// function deletePhotoCard() {
+//   var uniqueId = event.target.parentElement.parentElement.parentElement.dataset.id;
+//   var parsedId = parseInt(uniqueId);
+//   console.log(typeof parsedId);
+//   // parsedId.remove();
+//   var index = imagesArr.findIndex(function(photo) {
+//     return photo.id === parseInt(parsedId);
+//   });
+//   imagesArr[index].deleteFromStorage();
+// }
+
+function deletePhotoCard() {
+  var uniqueId = event.target.parentElement.parentElement.parentElement.dataset.id;
+  var parsedId = parseInt(uniqueId);
+  console.log(typeof parsedId);
+  // parsedId.remove();
+  var index = imagesArr.findIndex(function(photo) {
+    return photo.id === parseInt(parsedId);
+  });
+  imagesArr[index].deleteFromStorage();
+  reloadPhotoCards();
+}
+
+function reloadPhotoCards() {
+ Object.keys(localStorage).forEach(function(key) {
+  var thisCard = JSON.parse(localStorage.getItem(key))
+  appendPhotos(thisCard);
+  var newIdea = new Idea(thisCard.id, thisCard.title, thisCard.body, thisCard.quality);
+  imagesArr.push(newIdea);
+ })
+}
+// --------------------
